09de5e9753925ef229a92579d0cc5a4b51902b63
joshu <joshu@REEDPC>
2024-05-15 14:55:53 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Transactions;
+
+namespace Assignment2._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Calculator();
+        }
+
+        static void Calculator() 
+        {
+            Console.WriteLine("Enter the price of your meal");
+            double mealCost = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your gratuity percentage");
+            double myGratuity = Convert.ToInt32(Console.ReadLine());
+            double billTotal = myGratuity/100 * mealCost + mealCost; 
+            Console.WriteLine($"The total is: ${ billTotal}");
+            Console.ReadLine();
+        }
+
+    }
+}
